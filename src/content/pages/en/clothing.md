46b4846f6b423d3e5b8aee57af665eb4576701b5
ezriharmusial <ezriharmusial@gmail.com>
2024-05-26 16:06:41 +0200

    Restore

--- src/content/pages/en/clothing.md
+++ src/content/pages/en/clothing.md
@@ -9,6 +9,6 @@ tags:
   - restoration
 icon: hanger
 pack: mdi
-layout: ../../../layouts/page.astro
+layout: ../../../layouts/Page.astro
 ---
 🏗️ Under Construction 🚧
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,8 @@ module.exports = {
 	darkMode: 'class',
 	content: [
 		'./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+		require('path').join(require.resolve('@skeletonlabs/skeleton')),
+		'node_modules/preline/preline.js'
 	],
 	theme: {
 		extend: {},
@@ -11,5 +13,8 @@ module.exports = {
 		require('@tailwindcss/typography'),
 		require('@tailwindcss/forms'),
     	require('autoprefixer'),
+    	require('preline/plugin'),
+		// Keep any existing plugins present and append the following:
+		...require('@skeletonlabs/skeleton/tailwind/skeleton.cjs')()
 	],
 }
